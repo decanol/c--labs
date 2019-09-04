87081f3b9b23be9c4701b32f8fbd59b8cceea8c6
Oleh Moskaliuk <web.moskaliuk@gmail.com>
2019-09-04 12:53:51 +0300

    lab-1

--- main.cpp
+++ main.cpp
@@ -1,30 +1,106 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 class Product {
 public:
-    char title;
-    char firm;
+    string title;
+    string firm;
     float price;
     int date;
     int number;
-    Product(char title, char firm, float price, int date, int number) {
-        title = title;
-        firm = firm;
-        price = price;
-        date = date;
-        number = number;
+    Product() {
+        this->title = "N/A";
+        this->firm = "N/A";
+        this->price = 0.0;
+        this->date = 0;
+        this->number = 0;
+    }
+    Product(string title, string firm, float price, int date, int number) {
+        this->title = title;
+        this->firm = firm;
+        this->price = price;
+        this->date = date;
+        this->number = number;
+    }
+    void Change(int i){
+        string title; string firm; float price; int date; int number;
+        cout<<i<<" Object"<<endl;
+        cout<<"give title: ";
+        cin>>title;
+        cout<<"give firm: ";
+        cin>>firm;
+        cout<<"give price: ";
+        cin>>price;
+        cout<<"give date: ";
+        cin>>date;
+        cout<<"give number: ";
+        cin>>number;
+        this->title = title;
+        this->firm = firm;
+        this->price = price;
+        this->date = date;
+        this->number = number;
+    }
+    void Display() {
+        cout << this->title << endl;
+        cout << this->firm << endl;
+        cout << this->price << endl;
+        cout << this->date << endl;
+        cout << this->number << endl;
+        cout << endl;
+    }
+    void printByTitle(string title){
+        if(this->title == title){
+            this->Display();
+        }
+    }
+    void printFixedPriceProduct(string title, float price) {
+        if(this->title == title){
+            if(this->price <= price){
+                this->Display();
+            }
+        }
+    }
+
+    void printOverDateProduct(int date) {
+        if(this->date > date){}
+        this->Display();
     }
 };
 
-void printFixedPriceProduct() {
-
-}
-void printOverDateProduct() {
-
-}
-
 int main() {
-    Product milkProd("Butter", "Molokia", 15.99, 15, 60);
-    std::cout << milkProd.firm << " " << milkProd.title << " " << milkProd.number << "\n";
+
+    Product arrayOfProducts[8];
+    string title;
+    for (int i = 0; i < 4; ++i) {
+
+        arrayOfProducts[i].Change (i+1);
+    }
+    for (int i = 0; i < 4; ++i) {
+        arrayOfProducts[i].Display();
+    }
+    string checkTitle;
+    cout<<"Check Title: ";
+    cin>>checkTitle;
+    float checkPrice;
+    cout<<"Check Price: ";
+    cin>>checkPrice;
+    int checkDate;
+    cout<<"Check Date: ";
+    cin>>checkDate;
+    cout<<"Filter by title:"<<checkTitle;
+    for (int i = 0; i < 4; ++i) {
+        arrayOfProducts[i].printByTitle(checkTitle);
+    }
+    cout<<"Filter by price:"<<checkPrice;
+    for (int i = 0; i < 4; ++i) {
+        arrayOfProducts[i].printFixedPriceProduct(checkTitle, checkPrice);
+    }
+    cout<<"Filter by date:"<<checkDate;
+    for (int i = 0; i < 4; ++i) {
+        arrayOfProducts[i].printOverDateProduct(checkDate);
+    }
+
     return 0;
 }
